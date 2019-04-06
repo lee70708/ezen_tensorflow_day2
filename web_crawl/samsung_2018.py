832e952613914f34ef985034684bb0152325f371
lee70708 <lee70708@gmail.com>
2019-04-06 16:26:02 +0900

    삼성전자 단어분석

--- web_crawl/samsung_2018.py
+++ web_crawl/samsung_2018.py
@@ -1,9 +1,14 @@
 from konlpy.tag import Okt
 import re
 from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 # import nltk
 # nltk.download()
 
+okt = Okt()
 ctx = 'C:/Users/ezen/PycharmProjects/test2/data/'
 filename = ctx + 'kr-Report_2018.txt'
 
@@ -16,3 +21,32 @@ texts = tokenizer.sub('', texts)
 tokens = word_tokenize(texts)
 
 print(texts[:7])
+
+noun_token = []
+for token in tokens:
+    token_pos = okt.pos(token)
+    temp = [txt_tag[0] for txt_tag in token_pos if txt_tag[1] =="Noun"]
+    if len(''.join(temp)) > 1:
+        noun_token.append("".join(temp))
+texts = " ".join(noun_token)
+print(texts[:300])
+
+with open(ctx+'stopwords.txt', 'r', encoding='UTF-8') as f:
+    stopwords = f.read()
+
+stopwords = stopwords.split(' ')
+print(texts[:10])
+
+texts = [text for text in tokens if text not in stopwords]
+freqtxt = pd.Series(dict(FreqDist(texts))).sort_values(ascending=False)
+print(freqtxt[:30])
+
+okt.pos('가치창출')
+okt.pos('갤러시')
+
+wcloud = WordCloud(ctx + 'D2Coding.ttf', relative_scaling=0.2, background_color='white').generate(" ".join(texts))
+
+plt.figure(figsize=(12,12))
+plt.imshow(wcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
